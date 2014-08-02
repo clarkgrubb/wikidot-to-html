f65c2ab7b5bee75d81cf8a2adbe202c5d9da7a2c
Clark Grubb <clarkgrubb@gmail.com>
2014-08-02 12:44:09 -0700

    get test.phrase to pass

--- src/wikidot_to_html.py
+++ src/wikidot_to_html.py
@@ -29,40 +29,40 @@ MULTILINE_BLOCK_TYPES = [BLOCK_TYPE_P,
                          BLOCK_TYPE_OL,
                          BLOCK_TYPE_TABLE]
 
-REGEX_UL = re.compile(r'^(?P<indent>\s*)\*\s+(?P<content>\S.*)$')
-REGEX_OL = re.compile(r'^(?P<indent>\s*)\#\s+(?P<content>\S.*)$')
-REGEX_BLOCKQUOTE = re.compile(
+RX_UL = re.compile(r'^(?P<indent>\s*)\*\s+(?P<content>\S.*)$')
+RX_OL = re.compile(r'^(?P<indent>\s*)\#\s+(?P<content>\S.*)$')
+RX_BLOCKQUOTE = re.compile(
     r'^(?P<greater_than_signs>>+)\s*(?P<content>.*)$')
-REGEX_TABLE = re.compile(r'^(?P<indent>\s*)(?P<content>\|\|.*)$')
-REGEX_HN = re.compile(
+RX_TABLE = re.compile(r'^(?P<indent>\s*)(?P<content>\|\|.*)$')
+RX_HN = re.compile(
     r'^(?P<indent>\s*)(?P<plus_signs>\+{1,6})\s+(?P<content>\S.*)$')
-REGEX_HR = re.compile(r'^(?P<indent>\s*)----(?P<content>)$')
-REGEX_EMPTY = re.compile(r'^\s*$')
-REGEX_P = re.compile(r'^(?P<content>.*)$')
-REGEX_MARKERS = re.compile(r'(//|\*\*\|\{\{|\}\}|@@)')
-REGEX_WHITESPACE = re.compile(r'(\s+)')
+RX_HR = re.compile(r'^(?P<indent>\s*)----(?P<content>)$')
+RX_EMPTY = re.compile(r'^\s*$')
+RX_P = re.compile(r'^(?P<content>.*)$')
+RX_MARKERS = re.compile(r'(//|\*\*|\{\{|\}\}|@@)')
+RX_WHITESPACE = re.compile(r'(\s+)')
 
 
 def analyze_line(line):
-    md = REGEX_UL.search(line)
+    md = RX_UL.search(line)
     if md:
         return BLOCK_TYPE_UL, md
-    md = REGEX_OL.search(line)
+    md = RX_OL.search(line)
     if md:
         return BLOCK_TYPE_OL, md
-    md = REGEX_TABLE.search(line)
+    md = RX_TABLE.search(line)
     if md:
         return BLOCK_TYPE_TABLE, md
-    md = REGEX_HN.search(line)
+    md = RX_HN.search(line)
     if md:
         return BLOCK_TYPE_HN, md
-    md = REGEX_HR.search(line)
+    md = RX_HR.search(line)
     if md:
         return BLOCK_TYPE_HR, md
-    md = REGEX_EMPTY.search(line)
+    md = RX_EMPTY.search(line)
     if md:
         return BLOCK_TYPE_EMPTY, md
-    md = REGEX_P.search(line)
+    md = RX_P.search(line)
     if md:
         return BLOCK_TYPE_P, md
 
@@ -70,134 +70,216 @@ def analyze_line(line):
 
 
 class Node(object):
-    def __init__(self):
+    def __init__(self, raw_tag='', tag=''):
         self.children = []
+        self.raw_tag = raw_tag
+        self.tag = tag
+        self.closure = False
+
+    def set_closure(self, nd=True):
+        self.closure = nd
+
+    def closed(self):
+        if type(self.closure) == bool:
+            return self.closure
+        else:
+            return self.closure.closed()
 
     def __str__(self):
-        return ''.join([str(child) for child in self.children])
+        if len(self.children) == 0:
+            first = ''
+            rest = ''
+        elif len(self.children) == 1:
+            if self.children[0] == ' ':
+                first = ' '
+                rest = ''
+            else:
+                first = ''
+                rest = str(self.children[0])
+        else:
+            if self.children[0] == ' ':
+                first = ' '
+                rest = ''.join([str(child) for child in self.children[1:]])
+            else:
+                first = ''
+                rest = ''.join([str(child) for child in self.children])
+
+        if self.closed():
+            if rest:
+                return '{}<{}>{}</{}>'.format(first,
+                                              self.tag,
+                                              rest,
+                                              self.tag)
+            else:
+                return first
+        else:
+            return '{}{}{}{}'.format(first, self.raw_tag, rest, '')
 
 
-class Italic(object):
+class Italic(Node):
+    def __init__(self, raw_tag='//'):
+        Node.__init__(self, raw_tag, 'em')
+
+
+class Bold(Node):
+    def __init__(self, raw_tag='**'):
+        Node.__init__(self, raw_tag, 'strong')
+
+
+class FixedWidth(Node):
+    def __init__(self, raw_tag='{{'):
+        Node.__init__(self, raw_tag, 'tt')
+
+class LineBreak(Node):
     def __init__(self):
-        self.children = []
+        pass
 
     def __str__(self):
-        s = ''.join([str(child) for child in self.children])
-
-        return '<em>{}</em>'.format(s)
+        return '<br />\n'
 
 
-class Bold(object):
-    def __init__(self):
-        self.children = []
-
-    def __str__(self):
-        s = ''.join([str(child) for child in self.children])
-
-        return '<strong>{}</strong>'.format(s)
+LINE_BREAK = LineBreak()
 
 
-class FixedWidth(object):
-    def __init__(self):
-        self.children = []
+def lex(text):
+    words = RX_WHITESPACE.split(text)
+    tokens = []
+    for word in words:
+        tokens.extend(RX_MARKERS.split(word))
 
-    def __str__(self):
-        s = ''.join([str(child) for child in self.children])
-
-        return '<tt>{}</tt>'.format(s)
+    return [token for token in tokens if token]
 
 
-class Phrase(object):
+class PhraseParser(object):
 
     def __init__(self):
         self.italic = False
         self.bold = False
         self.literal = False
         self.fixed_width = False
-        self.nodes = None
+        self.top_node = Node()
+        self.nodes = [self.top_node]
 
-    def _adjust_flags(self, cls):
+    def __str__(self):
+        return str(self.top_node)
+
+    def set_flag(self, cls, value):
         if cls == Italic:
-            self.italic = False
+            self.italic = value
         elif cls == Bold:
-            self.bold = False
+            self.bold = value
         elif cls == FixedWidth:
-            self.fixed_width = False
+            self.fixed_width = value
+        elif cls == Node:
+            pass
         else:
-            raise Exception('bam: {}'.format(cls))
+            raise Exception('unknown class: {}'.format(cls))
 
-    def _remove_node(self, cls):
-        while self.nodes and type(self.nodes[-1]) != cls:
-            self._adjust_flags(type(self.nodes.pop()))
-        if self.nodes and type(self.nodes[-1]) == cls:
-            self._adjust_flags(type(self.nodes.pop()))
-        else:
-            raise Exception('no node of type {} on stack'.format(cls))
+    def remove_flag(self, cls):
+        self.set_flag(cls, False)
 
-    def render(self, content):
-        words = REGEX_WHITESPACE.split(content)
-        tokens = []
-        for word in words:
-            tokens.extend(REGEX_MARKERS.split(word))
-        top_node = Node()
-        self.nodes = [top_node]
-        tokens = [token for token in tokens if token]
+    def add_flag(self, cls):
+        self.set_flag(cls, True)
 
+    def remove_all_nodes(self):
+        removed_nodes = []
+        while self.nodes:
+            nd = self.nodes.pop()
+            removed_nodes.append(nd)
+            self.remove_flag(type(nd))
+
+        return removed_nodes
+
+    def restore_all_nodes(self, removed_nodes):
+        self.top_node = type(removed_nodes.pop())()
+        self.nodes = [self.top_node]
+        self.restore_nodes(removed_nodes)
+
+    def restore_nodes(self, removed_nodes):
+        while removed_nodes:
+            old_nd = removed_nodes.pop()
+            new_nd = type(old_nd)('')
+            old_nd.set_closure(new_nd)
+            self.add_node(new_nd)
+
+    def remove_nodes_to_class(self, cls_to_remove):
+        removed_nodes = []
+        while True:
+            if not self.nodes:
+                raise Exception('not on stack: {}'.format(cls_to_remove))
+            nd = self.nodes.pop()
+            removed_nodes.append(nd)
+            self.remove_flag(type(nd))
+            if type(nd) == cls_to_remove:
+                break
+
+        return removed_nodes
+
+    def remove_node(self, cls_to_remove):
+        removed_nodes = self.remove_nodes_to_class(cls_to_remove)
+        nd = removed_nodes.pop()
+        self.restore_nodes(removed_nodes)
+
+        return nd
+
+    def add_node(self, nd):
+        self.nodes[-1].children.append(nd)
+        self.nodes.append(nd)
+        self.add_flag(type(nd))
+
+    def add_text(self, s):
+        self.nodes[-1].children.append(s)
+
+    def parse(self, tokens):
         for i, token in enumerate(tokens):
-            if REGEX_WHITESPACE.match(token):
-                self.nodes[-1].children.append(' ')
+            if RX_WHITESPACE.match(token):
+                self.add_text(' ')
             elif token == '//':
                 if self.italic:
-                    if i > 0 and not REGEX_WHITESPACE.match(tokens[i - 1]):
-                        self._remove_node(Italic)
+                    if i > 0 and not RX_WHITESPACE.match(tokens[i - 1]):
+                        nd = self.remove_node(Italic)
+                        nd.set_closure(True)
                     else:
-                        self.nodes[-1].children.append('//')
+                        self.add_text('//')
                 elif not self.italic:
                     if i < len(tokens) - 1 and \
-                       not REGEX_WHITESPACE.match(tokens[i + 1]):
-                        nd = Italic()
-                        self.nodes[-1].children.append(nd)
-                        self.nodes.append(nd)
-                        self.italic = True
+                       not RX_WHITESPACE.match(tokens[i + 1]):
+                        self.add_node(Italic())
                     else:
-                        self.nodes[-1].children.append('//')
+                        self.add_text('//')
             elif token == '**':
                 if self.bold:
-                    if i > 0 and not REGEX_WHITESPACE.match(tokens[i - 1]):
-                        self._remove_node(Bold)
+                    if i > 0 and not RX_WHITESPACE.match(tokens[i - 1]):
+                        nd = self.remove_node(Bold)
+                        nd.set_closure(True)
                     else:
-                        self.nodes[-1].children.append('@@')
+                        self.add_text('**')
                 elif not self.bold:
                     if i < len(tokens) - 1 and \
-                       not REGEX_WHITESPACE.match(tokens[i + 1]):
-                        nd = Bold()
-                        self.nodes[-1].children.append(nd)
-                        self.nodes.append(nd)
-                        self.bold = True
+                       not RX_WHITESPACE.match(tokens[i + 1]):
+                        self.add_node(Bold())
                     else:
-                        self.nodes[-1].children.append('@@')
+                        self.add_text('**')
             elif token == '@@':
-                self.nodes[-1].children.append('@@')
+                self.add_text('@@')
             elif token == '{{':
                 if not self.fixed_width:
                     if i < len(tokens) - 1 and \
-                       not REGEX_WHITESPACE.match(tokens[i + 1]):
-                        nd = FixedWidth()
-                        self.nodes[-1].children.append(nd)
-                        self.nodes.append(nd)
-                        self.fixed_width = True
+                       not RX_WHITESPACE.match(tokens[i + 1]):
+                        self.add_node(FixedWidth())
                     else:
-                        self.nodes[-1].children.append('{{')
+                        self.add_text('{{')
             elif token == '}}':
                 if self.fixed_width:
-                    if i > 0 and not REGEX_WHITESPACE.match(tokens[i - 1]):
-                        self._remove_node(FixedWidth)
+                    if i > 0 and not RX_WHITESPACE.match(tokens[i - 1]):
+                        nd = self.remove_node(FixedWidth)
+                        nd.set_closure(True)
                     else:
-                        self.nodes[-1].children.append('}}')
+                        self.add_text('}}')
             else:
-                self.nodes[-1].children.append(cgi.escape(token))
+                self.add_text(cgi.escape(token))
 
-        return str(top_node)
+        return self.top_node
 
 
 class Block(object):
@@ -225,7 +307,6 @@ class Block(object):
         if self.block_type == BLOCK_TYPE_HN:
             len_plus_signs = len(self.matches[0].group('plus_signs'))
             return 'h{}'.format(len_plus_signs)
-
         return self.block_type
 
     def multiline_type(self):
@@ -236,13 +317,14 @@ class Block(object):
 
     def write_content(self, phrase, output_stream):
         for match in self.matches:
-            output_stream.write(phrase.render(match.group('content')))
+            phrase.parse(lex(match.group('content')))
+            output_stream.write(str(phrase.top_node))
 
     def write_close_tag(self, output_stream):
         output_stream.write('</{}>\n'.format(self.tag))
 
     def close(self, output_stream):
-        phrase = Phrase()
+        phrase = PhraseParser()
         self.write_open_tag(output_stream)
         self.write_content(phrase, output_stream)
         self.write_close_tag(output_stream)
@@ -264,10 +346,17 @@ class UnorderedList(Block):
         output_stream.write('<{}>\n'.format(self.tag))
 
     def write_content(self, phrase, output_stream):
+        top_nodes = []
         for match in self.matches:
+            phrase.parse(lex(match.group('content')))
+            top_nodes.append(phrase.top_node)
+            removed_nodes = phrase.remove_all_nodes()
+            phrase.restore_all_nodes(removed_nodes)
+        for top_node in top_nodes:
             output_stream.write('<li>')
-            output_stream.write(phrase.render(match.group('content')))
+            output_stream.write(str(top_node))
             output_stream.write('</li>\n')
+
 
 
 class OrderedList(Block):
@@ -280,8 +369,11 @@ class OrderedList(Block):
     def write_content(self, phrase, output_stream):
         for match in self.matches:
             output_stream.write('<li>')
-            output_stream.write(phrase.render(match.group('content')))
+            phrase.parse(lex(match.group('content')))
+            output_stream.write(str(phrase.top_node))
+            removed_nodes = phrase.remove_all_nodes()
             output_stream.write('</li>\n')
+            phrase.restore_all_nodes(removed_nodes)
 
 
 class Empty(Block):
@@ -297,11 +389,13 @@ class Paragraph(Block):
     def __init__(self, line, match):
         Block.__init__(self, line, BLOCK_TYPE_P, match)
 
-    def write_content(self, phrase, output_stream):
-        content = [phrase.render(match.group('content'))
-                   for match
-                   in self.matches]
-        output_stream.write('<br />\n'.join(content))
+    def write_content(self, parser, output_stream):
+        tokens = []
+        for i, match in enumerate(self.matches):
+            parser.parse(lex(match.group('content')))
+            if i < len(self.matches) - 1:
+                parser.add_text(LINE_BREAK)
+        output_stream.write(str(parser.top_node))
 
 
 def block_factory(line, block_type=None, match=None):
@@ -320,7 +414,7 @@ def block_factory(line, block_type=None, match=None):
 
 
 def adjust_blockquote_level(output_stream, current_block, line, bq_level):
-    md = REGEX_BLOCKQUOTE.search(line)
+    md = RX_BLOCKQUOTE.search(line)
     if md:
         new_bq_level = len(md.group('greater_than_signs'))
         line = md.group('content')
